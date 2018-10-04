bd06aa485305ef86a925b736b4dff9c97281dadd
Foscat <40616718+Foscat@users.noreply.github.com>
2018-10-04 14:19:02 -0500

    so far
    current time good and table fully updating just no working conversion math

--- assets/schedule.js
+++ assets/schedule.js
@@ -1,11 +1,6 @@
 //Needs to pull up train times using firebase
 //Somehow get actual train times and display thier arrival in real time
 
-//setup global varibles for
-var trainName = "";
-var destination = "";
-var nextArrivalTime = "";
-var frequency;
 
   // Initialize Firebase
   var config = {
@@ -20,72 +15,107 @@ var frequency;
 
 var database = firebase.database();
 
+
+
 // Setup global varibles
 var trainName = "";
 var destination = "";
-var firstTrain;
-var frequency;
+var firstTrain = "";
+var frequency = "";
+var nextArrival = "next time";
+var minAway = "min away";
 
   
 //When the submit button is pushed
 $("#add-Train").on('click', function(event) {
   
   event.preventDefault();
-
-  //this grabs these values and the information is pushed 
+  
+  //This grabs these values in this format when information is pushed 
   trainName = $("#trainNameInput").val().trim();
   destination = $("#destinationInput").val().trim();
-  firstTrain = moment($("#firstTrainInput").val().trim(), "MM/DD/YYYY").format("X");
+  firstTrain = moment($("#firstTrainInput").val().trim(), "HH:mm Military Time").format("LT");
   frequency = $("#frequencyInput").val().trim();
 
+  // Makes sure all inputs have a value beore submitting to table
+  if (trainName === ""){
+    alert("Enter train name");
+    return false;
+  }
+  if (destination === ""){
+    alert("Enter destination");
+    return false;
+  }
+  if (firstTrain === ""){
+    alert("Enter first train time");
+    return false;
+  }
+  if (frequency === ""){
+    alert("Enter a frequency");
+    return false;
+  }
+
+  //Create a var to keep track of current time and control its format
+  var currentTime = moment().format("LT");
+
+  //Create a var that does the math between 
+  //Subtract the first train time back a year to make sure it's before current time.
+  //var firstTrianConversion = moment(firstTrain, "HH:mm").subtract("1, years");
+
+  //Differance in time between current time and first train
+  //var timeDiff = currentTime - firstTrianConversion;
+
+  // % = Modulus (Remainder)
+  //var remainder = timeDiff % frequency;
+
+  //Time math to get minuites until the next train
+  //var minUntilTrain = frequency - remainder; 
+ // var nextTrain = moment().add(minUntilTrain, "minutes").format("HH:mm Military Time");
+
+  
   console.log(trainName);
   console.log(destination);
   console.log(firstTrain);
-  console.log(frequency);
+  console.log(frequency);  
+  console.log(currentTime);
+  // console.log(firstTrianConversion);
+  // console.log(timeDiff);
+  // console.log(remainder);
+  // console.log(minUntilTrain);
+  //console.log(nextTrain);
+
 
 
   database.ref().push({
     trainName: trainName,
     destination: destination,
     firstTrain: firstTrain,
-    frequency: frequency
+    frequency: frequency,
+    currentTime: currentTime,
+    //min: minUntilTrain,
+    //next: nextTrain
     });
 
 });
 
 // Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
-database.ref().on("child_added", function(childSnapshot){
+database.ref().on("child_added", function(snapshot){
 
-
-  // Store everything into a variable.
-  var trnName = (childSnapshot.val().trainName);
-  var dest = (childSnapshot.val().destination);
-  var fTrn = (childSnapshot.val().firstTrain);
-  var freak = (childSnapshot.val().frequency);
-
-  // train Info
-  console.log(trnName);
-  console.log(dest);
-  console.log(fTrn);
-  console.log(freak);
-
-  $("#train-table").append("<tr class='train-info-row'><td class='train-info-name> " +
-   trnName + "</td><td class='train-info-destination'>" + dest + "</td><td class='train-info-first'>" +
-   fTrn + "</td><td class='train-info-frequency'>" + freak + "</td></tr>");
+  var sv = snapshot.val();
+  console.log(sv.trainName);
+  console.log(sv.destination);
+  console.log(sv.firstTrain);
+  console.log(sv.frequency);
+ 
+  $("#train-table").append("<tr class='train-info-row'>" + 
+  "<td class='train-info-name'>" + sv.trainName + "</td>" +
+  "<td class='train-info-destination'>" + sv.destination + "</td>" + 
+  "<td class='train-info-frequency'>" + sv.frequency + "</td>" + 
+  "<td class='train-info-next'>" + nextArrival + "</td>" + 
+  "<td class='train-info-away'>" + minAway +"</td>" + "</tr>");
 
   
 }, function(errorObject) {
   console.log("Errors handled: " + errorObject.code);
 });
 
-// //put train info into a  table row
-  // var newRow = $("<tr>").append(
-  //   $("<td>").text(trnName),
-  //   $("<td>").text(dest),
-  //   $("<td>").text(fTrn),
-  //   $("<td>").text(freak),
-    
-  // );
-
-  // // Append the new row to the table
-  // $("#train-table > tbody").append(newRow);
